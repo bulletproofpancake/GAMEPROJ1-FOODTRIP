5c00719ec5cc2f2542b43174e9b6fbec175288eb
jolo <jgutierrez1100@gmail.com>
2021-02-26 21:07:05 +0800

    VN transition implemented

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -20,10 +20,12 @@ public class GameManager : MonoBehaviour
     {
         if (isVN)
         {
+            arcadeCanvas.SetActive(false);
             Instantiate(vnCanvas, transform.position, Quaternion.identity);
         }
         else
         {
+            vnCanvas.SetActive(false);
             Instantiate(arcadeCanvas, transform.position, Quaternion.identity);
         }
 
@@ -44,7 +46,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isVN && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneSelector.Instance.LoadNextScene();
+        }
     }
 
     private IEnumerator CountDownLevel()
