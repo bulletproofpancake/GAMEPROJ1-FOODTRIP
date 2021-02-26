4f7624e7659dc1121c02ec31b08aba8449e07f93
jolo <jgutierrez1100@gmail.com>
2021-02-26 21:00:58 +0800

    Scene transitions implemented in arcade

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Round Settings")]
+    [SerializeField] private bool isVN;
+    [SerializeField] private GameObject arcadeCanvas;
+    [SerializeField] private GameObject vnCanvas;
+    [SerializeField] private GameObject cartUsed;
+    [SerializeField] private int levelDuration;
+    
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI moneyText;
+    
+    private void Awake()
+    {
+        if (isVN)
+        {
+            Instantiate(vnCanvas, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(arcadeCanvas, transform.position, Quaternion.identity);
+        }
+
+        Instantiate(cartUsed, transform.position, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneSelector.Instance.transition.Play("Crossfade_End");
+        if (!isVN)
+        {
+            StartCoroutine(CountDownLevel());
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator CountDownLevel()
+    {
+        while (levelDuration > 0)
+        {
+            timerText.text = $"{levelDuration}";
+            yield return new WaitForSeconds(1f);
+            levelDuration--;
+        }
+        SceneSelector.Instance.LoadNextScene();
+    }
+}
