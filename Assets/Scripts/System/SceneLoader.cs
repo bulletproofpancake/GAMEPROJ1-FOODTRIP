8f7189d444f182029cf8682788d02250b2c82e98
jolo <jgutierrez1100@gmail.com>
2021-02-26 19:10:58 +0800

    basic scene selector implemented

---
+++ Assets/Scripts/System/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneSelector.Instance.LoadNextScene(0);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneSelector.Instance.LoadNextScene();
+    }
+
+    public void LoadPreviousScene()
+    {
+        SceneSelector.Instance.LoadPreviousScene();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
+}
---
+++ Assets/Scripts/System/SceneSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : Singleton<SceneSelector>
+{
+    public void LoadPreviousScene()
+    {
+        if(SceneManager.GetActiveScene().buildIndex - 1 >= 0){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        else
+        {
+            print("No Previous Scene Available");
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadNextScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
