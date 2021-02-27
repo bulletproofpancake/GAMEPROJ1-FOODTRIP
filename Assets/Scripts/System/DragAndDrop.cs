5dc29b2b756ac43ad92070c5e1fdeab32d089738
jolo <jgutierrez1100@gmail.com>
2021-02-27 13:47:17 +0800

    Drag and drop added

---
+++ Assets/Scripts/System/DragAndDrop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+//following: https://youtu.be/55TBhlOt_U8
+public class DragAndDrop : MonoBehaviour
+{
+    private bool _isDragging;
+    private Camera _camera;
+    private Vector2 _mousePos;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    public void OnMouseDown()
+    {
+        _isDragging = true;
+    }
+    
+    public void OnMouseUp()
+    {
+        _isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (!_isDragging) return;
+        _mousePos = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        transform.Translate(_mousePos);
+    }
+
+    private void OnDisable()
+    {
+        OnMouseUp();
+    }
+}
