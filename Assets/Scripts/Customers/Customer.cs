607cdd99c76a3a8eebcfeaa64a71fb208d55ab06
jolo <jgutierrez1100@gmail.com>
2021-02-27 16:35:14 +0800

    order box added

--- Assets/Scripts/Customers/Customer.cs
+++ Assets/Scripts/Customers/Customer.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 public class Customer : MonoBehaviour
 {
     [SerializeField] private CustomerData data;
+    [SerializeField] private GameObject orderIcon;
+    [SerializeField] private Order currentOrder;
+    
     private SpriteRenderer _spriteRenderer;
+    
     public int SeatTaken { get; set; }
     
-    private void Start()
+    private void OnEnable()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.sprite = data.ChangeSprite();
+        GiveOrder();
     }
-    
 
     private void OnDisable()
     {
         SpawnManager.Instance.customerSeat[SeatTaken].isTaken = false;
+    }
+
+    private void GiveOrder()
+    {
+        currentOrder = data.SelectOrder();
+        orderIcon.GetComponent<SpriteRenderer>().sprite = currentOrder.Data.Image;
     }
 }
