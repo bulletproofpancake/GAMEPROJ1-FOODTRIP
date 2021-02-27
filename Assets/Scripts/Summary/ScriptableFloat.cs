f7e415765499c00976fdfe8d5c2eae146d734ed1
andrei-excel <andrei.excel2@gmail.com>
2021-02-27 19:32:28 +0800

    Initial implementation of data manager

---
+++ Assets/Scripts/Summary/ScriptableFloat.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableFloat", menuName = "Data/New Float")]
+public class ScriptableFloat : ScriptableObject
+{
+    [SerializeField] private float value;
+
+    public float Value
+    {
+        get => value;
+        set => this.value = value;
+    }
+}
---
+++ Assets/Scripts/System/DataManager/DataManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : Singleton<DataManager>
+{
+    //public PlayerStatistics savedPlayerData = new PlayerStatistics();
+
+    // Variables for summary screen
+    // These reset every round;
+    [SerializeField] ScriptableFloat actualMoney;
+    [SerializeField] ScriptableFloat expectedMoney;
+    [SerializeField] ScriptableFloat customersServed;
+
+
+    [SerializeField] private float totalMoney;
+
+    public float PlayerTotalMoney
+    {
+        get { return totalMoney; }
+        set { totalMoney = value; }
+    }
+
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        //PlayerTotalMoney = 200;
+
+        // Resets the summary screen variables every new round
+        actualMoney.Value = 0;
+        expectedMoney.Value = 0;
+        customersServed.Value = 0;
+    }
+}
+
