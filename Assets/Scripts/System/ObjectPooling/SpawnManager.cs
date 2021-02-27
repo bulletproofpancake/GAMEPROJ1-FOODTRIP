54db78d8f8b0ed3e04da0566c40e70c1fbf20ac3
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-02-27 10:18:24 +0800

    Customer Spawning (#4)
    * Simple Customer Spawning Implemented
    
    * Worker added
    
    * Customer Spawning technically works

--- Assets/Scripts/System/ObjectPooling/SpawnManager.cs
+++ Assets/Scripts/System/ObjectPooling/SpawnManager.cs
@@ -1,20 +1,86 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : Singleton<SpawnManager>
 {
+    [Header("Customer Spawn")]
+    public int spawnInterval;
+    public CustomerData[] customers;
+    public Seat[] customerSeat;
+    private int _customerIndex;
+    private bool _allowedToSpawn;
+    
+    [Header("Food Spawn")]
     public Seat[] foodSeat;
     private int _foodIndex;
 
     private void Start()
     {
+        foreach (var seat in customerSeat)
+        {
+            seat.isTaken = false;
+        }
         foreach (var seat in foodSeat)
         {
             seat.isTaken = false;
         }
+
+        StartCoroutine(SpawnCustomers());
+
     }
     
+    public void Spawn(CustomerData data)
+    {
+        var customer = ObjectPoolManager.Instance.GetPooledObject(data.name);
+        _customerIndex = Random.Range(0, customerSeat.Length);
+        
+        if (isFull()) return;
+        
+        if (!customerSeat[_customerIndex].isTaken)
+        {
+            customer.transform.position = customerSeat[_customerIndex].slot.position;
+            customer.GetComponent<Customer>().SeatTaken = _customerIndex;
+            customer.SetActive(true);
+            customerSeat[_customerIndex].isTaken = true;
+        }
+        else
+        {
+            Debug.LogWarning("Seat taken, looking for another");
+        }
+    }
+
+    private void Update()
+    {
+        if (_allowedToSpawn)
+        {
+            Spawn(customers[Random.Range(0,customers.Length)]);
+        }
+    }
+
+    private bool isFull()
+    {
+        var counter = 0;
+        
+        foreach (var seat in customerSeat)
+        {
+            if (seat.isTaken)
+            {
+                counter++;
+            }
+        }
+
+        if (counter >= customerSeat.Length)
+            return true;
+        else
+            return false;
+    }
+
+    private IEnumerator SpawnCustomers()
+    {
+        yield return new WaitForSeconds(spawnInterval);
+        _allowedToSpawn = true;
+    }
+
     public void Spawn(OrderData data)
     {
         var food = ObjectPoolManager.Instance.GetPooledObject(data.name);
