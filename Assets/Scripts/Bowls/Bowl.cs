508fc4eda873ce0ecfce0acae009f49e5380f633
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-02-27 12:22:18 +0800

    Bowl Spawning (#5)
    * bowl spawn implemented
    
    * bowl spawning fixed

---
+++ Assets/Scripts/Bowls/Bowl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bowl : MonoBehaviour
+{
+    public bool isDirty;
+    public float washTime;
+    public int SeatTaken { get; set; }
+    
+    private void OnEnable()
+    {
+        BowlSpawner.Instance.RemoveBowl(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        SpawnManager.Instance.bowlSeat[SeatTaken].isTaken = false;
+        if(isDirty)
+            Sink.Instance.AddBowl(gameObject);
+        else
+            BowlSpawner.Instance.AddBowl(gameObject); 
+    }
+}
