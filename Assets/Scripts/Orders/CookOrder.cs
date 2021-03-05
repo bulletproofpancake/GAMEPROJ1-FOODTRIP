ca2c1450900a6597826db47fc53e495336a003af
Jaxeetee <66991639+Jaxeetee@users.noreply.github.com>
2021-03-05 10:58:09 +0800

    Added Tusok Tusok
    - located at tusoktusokTestScene
    - added temporary sprites for the given gameObjects
    - done with garbage mechanic
    - still on the process of food spawn
    _ still on the process of limiting food spawn
    
    Minor changes
    - change the location of the bowl prefab since it was out of the group folder, Pares.

--- Assets/Scripts/Orders/CookOrder.cs
+++ Assets/Scripts/Orders/CookOrder.cs
@@ -6,14 +6,32 @@ public class CookOrder : MonoBehaviour
 {
     [SerializeField] private Order orderToCook;
 
+    #region Pares
     public void StartCooking()
     {
         StartCoroutine(Cook());
     }
+
 
     private IEnumerator Cook()
     {
         yield return new WaitForSeconds(orderToCook.Data.CookTime);
         SpawnManager.Instance.Spawn(orderToCook.Data);
     }
+
+    #endregion
+
+    #region TusokTusok
+
+    public void SpawnFood() {
+
+        StartCoroutine(AddFood());
+    }
+    private IEnumerator AddFood() {
+        yield return new WaitForSeconds(orderToCook.Data.CookTime);
+
+    }
+
+
+    #endregion
 }
---
+++ Assets/TusokTusokFoodSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TusokTusokFoodSpawner : Singleton<TusokTusokFoodSpawner>
+{
+    [Tooltip("this is for the area for the food to spawn along x-axis")]
+    [SerializeField]
+    [Range(0f, 2f)]
+    private float SpawnRange;
+
+    [Tooltip("Amount of same food to spawn per click of button")]
+    [Space]
+    [SerializeField]
+    private int batchToSpawn;
+    public void Spawn(OrderData data) {
+
+        var food = ObjectPoolManager.Instance.GetPooledObject(data.name);
+
+        food.transform.position = new Vector2(Random.Range(-SpawnRange, SpawnRange), this.gameObject.transform.position.y);
+        food.SetActive(true);
+
+    }
+}
