03fea9d78c6936df33e58ccc6e3b0236ba5430f6
jolo <jgutierrez1100@gmail.com>
2021-03-15 20:12:28 +0800

    area select now has the correct location sprites

--- Assets/Scripts/Shifts/CartData.cs
+++ Assets/Scripts/Shifts/CartData.cs
@@ -9,6 +9,8 @@ public class CartData : ScriptableObject
     public CartType Type => type;
     [SerializeField] private GameObject cart;
     public GameObject Cart => cart;
+    [SerializeField] private LocationSprites locSprites;
+    public LocationSprites LocSprites => locSprites;
 }
 
 public enum CartType
--- Assets/Scripts/Shifts/ShiftLoader.cs
+++ Assets/Scripts/Shifts/ShiftLoader.cs
@@ -2,14 +2,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShiftLoader : SceneLoader
 {
     [SerializeField] private ShiftData data;
+    [SerializeField] private ShiftSchedule schedule;
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+        SetBackgroundImage();
+    }
 
     public override void LoadNextScene()
     {
         ShiftManager.Instance.shift = data;
         base.LoadNextScene($"Scenes/Game Scenes/{ShiftManager.Instance.cart.Type}/Arcade");
+    }
+
+    private void SetBackgroundImage()
+    {
+        switch (ShiftManager.Instance.cart.Type)
+        {
+            case CartType.Paresan:
+                SetSprites();
+                break;
+            case CartType.Ihawan:
+                SetSprites();
+                break;
+            case CartType.Tusoktusok:
+                SetSprites();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void SetSprites()
+    {
+        switch (schedule)
+        {
+            case ShiftSchedule.Morning:
+                _image.sprite = ShiftManager.Instance.cart.LocSprites.Morning;
+                break;
+            case ShiftSchedule.Afternoon:
+                _image.sprite = ShiftManager.Instance.cart.LocSprites.Afternoon;
+                break;
+            case ShiftSchedule.Night:
+                _image.sprite = ShiftManager.Instance.cart.LocSprites.Night;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
