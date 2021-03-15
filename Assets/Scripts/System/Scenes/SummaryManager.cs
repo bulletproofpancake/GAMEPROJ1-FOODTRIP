170c1e8062e3cfd037e50e356d6b10cc9c24c451
jolo <jgutierrez1100@gmail.com>
2021-03-15 10:47:44 +0800

    summary now changes BG depending on shift

--- Assets/Scripts/System/Scenes/SummaryManager.cs
+++ Assets/Scripts/System/Scenes/SummaryManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SummaryManager : MonoBehaviour
 {
+    [SerializeField] private Image image;
     [SerializeField] private TextMeshProUGUI customersServed;
     [SerializeField] private TextMeshProUGUI expectedProfit;
     [SerializeField] private TextMeshProUGUI actualProfit;
@@ -13,7 +15,28 @@ public class SummaryManager : MonoBehaviour
 
     private void Start()
     {
+        if(ShiftManager.Instance.Data != null)
+            SetBackgroundImage();
+        
         actualProfit.text = $"Actual Profit: {MoneyManager.Instance.roundMoney}";
         totalMoney.text = $"Total Money: {MoneyManager.Instance.totalMoney}";
+    }
+
+    private void SetBackgroundImage()
+    {
+        switch (ShiftManager.Instance.Data.Schedule)
+        {
+            case ShiftSchedule.Morning:
+                image.sprite = ShiftManager.Instance.Data.LocSprites.Morning;
+                break;
+            case ShiftSchedule.Afternoon:
+                image.sprite = ShiftManager.Instance.Data.LocSprites.Afternoon;
+                break;
+            case ShiftSchedule.Night:
+                image.sprite = ShiftManager.Instance.Data.LocSprites.Night;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
