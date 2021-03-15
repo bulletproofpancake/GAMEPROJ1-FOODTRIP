730cf32b16ab168db625daf3ea6badbfbbddca77
andrei-excel <andrei.excel2@gmail.com>
2021-03-15 08:13:51 +0800

    Added button variable in InfoDisplay script

--- Assets/Scripts/System/Core/InfoDisplay.cs
+++ Assets/Scripts/System/Core/InfoDisplay.cs
@@ -2,28 +2,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InfoDisplay : MonoBehaviour
 {
     [SerializeField] private GameObject infoBox;
+    //private Button button;
 
     private void Start()
     {
+        //button = gameObject.GetComponent<Button>();
+
         infoBox.SetActive(false);
     }
+
 
     public void ShowInfo()
     {
         if (Pause.isPaused == true)// Closes Info Box
         {
             Pause.isPaused = false;
-            LeanTween.scale(infoBox, new Vector3(0, 0, 0), 0.5f);
             infoBox.SetActive(false);
+            print("Info open (PAUSED) = " + Pause.isPaused);
         }
         else// Opens Info Box
         {
             Pause.isPaused = true;
             infoBox.SetActive(true);
+            print("Info open (PAUSED) = " + Pause.isPaused);
         }
     }
 }
