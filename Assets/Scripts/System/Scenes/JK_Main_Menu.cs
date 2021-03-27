242c670b65b1e01fbb2046238fbee65a5722730d
JK <lexuskrav@gmail.com>
2021-03-27 11:17:30 +0800

    Main Menu JK Additions
    Added setActive false on canvas to avoid possible conflicts while opening game

--- Assets/Scripts/System/Scenes/JK_Main_Menu.cs
+++ Assets/Scripts/System/Scenes/JK_Main_Menu.cs
@@ -9,9 +9,18 @@ public class JK_Main_Menu : SceneLoader
     [SerializeField] GameObject ShiftSelect;
     bool isActive;
 
+    //Hard set to avoid game opening conflict bugs
+    private void Awake()
+    {
+        AreaSelect.SetActive(false);
+        ShiftSelect.SetActive(false);
+    }
+
+    //Simplest way of opening and closing a canvas
     public void AreaSelectMenu()
     {
         FindObjectOfType<AudioManager>().Play("ButtonClick");
+        Tween();
         if (AreaSelect != null)
         {
             isActive = AreaSelect.activeSelf;
@@ -22,11 +31,22 @@ public class JK_Main_Menu : SceneLoader
     public void ShiftSelectMenu()
     {
         FindObjectOfType<AudioManager>().Play("ButtonClick");
+        Tween();
         if (ShiftSelect != null)
         {
             ShiftManager.Instance.cart = data;
             isActive = ShiftSelect.activeSelf;
             ShiftSelect.SetActive(!isActive);
         }
+    }
+
+    void Tween()
+    {
+        LeanTween.scale(gameObject, new Vector3(.7f, .7f, .7f), 1f).setEase(LeanTweenType.punch);
+    }
+
+    void Slide()
+    {
+
     }
 }
